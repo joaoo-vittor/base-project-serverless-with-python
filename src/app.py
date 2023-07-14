ff8936f87afd7422d1f4656f7fb3fccfdb431fd9
joaoo-vittor <joaoo.vittor007@gmail.com>
2023-07-14 13:13:57 -0300

    chore: add schedule example

--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
-from json import loads
+import logging
+from json import loads, dumps
 from src.utils import make_response, valid_body
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def example_get(event, context):
     return make_response(200, { "message": "Hello World" })
+
+def scheduling(event, context):
+    
+    logger.info("[Message]: Hello World")
+   
+    return dumps({"message": "Hello World"})
 
 
 def example_post(event, context):
